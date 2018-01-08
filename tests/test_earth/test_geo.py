138e3b6e4a93b4783f75feb9d48f2c19fbd6a4fb
Bart Vleminckx <bart.vleminckx@observatory.be>
2018-01-08 13:06:34 +0100

    Add test for lon and lat validation

--- tests/test_earth/test_geo.py
+++ tests/test_earth/test_geo.py
@@ -5,4 +5,27 @@ Tests for 'earth.geo' module.
 
 import unittest
 
+import numpy as np
+
 from synthacc.earth.geo import Path, SphericalEarth, is_lon, is_lat
+
+
+class Test(unittest.TestCase):
+    """
+    """
+
+    def test_is_lon(self):
+        """
+        """
+        self.assertTrue(is_lon(5.1))
+        self.assertTrue(is_lon(-145.1))
+        self.assertFalse(is_lon(180.1))
+        self.assertTrue(is_lon(np.array([5.1, -145.1])))
+
+    def test_is_lat(self):
+        """
+        """
+        self.assertTrue(is_lat(1.2))
+        self.assertTrue(is_lat(-89.3))
+        self.assertFalse(is_lat(90.1))
+        self.assertTrue(is_lat(np.array([1.2,  -89.3])))
