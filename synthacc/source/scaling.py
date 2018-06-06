8540ff0ea0c15f8439cea2349034585e9339b896
Bart Vleminckx <bart.vleminckx@observatory.be>
2018-06-06 09:42:30 +0200

    Update 'source.scaling' module

--- synthacc/source/scaling.py
+++ synthacc/source/scaling.py
@@ -5,72 +5,82 @@ The 'source.scaling' module.
 
 import numpy as np
 
-
-def WC1994N_rl_to_m(rl):
-    """
-    See Wells & Coppersmith (1994).
-    """
-    return (4.86 + 1.32 * np.log10(rl/1000), 0.28)
+from .moment import mw_to_m0
 
 
-def WC1994A_rl_to_m(rl):
+def WC1994_srl_to_m(srl):
     """
-    See Wells & Coppersmith (1994).
+    Relation between surface rupture length and magnitude.
+
+    See Wells & Coppersmith (1994) p. 990 (r=0.89).
     """
-    return (5.08 + 1.16 * np.log10(rl/1000), 0.28)
+    return (5.08 + 1.16 * np.log10(srl/1000), 0.28)
 
 
-def WC1994N_m_to_ad(m):
+def WC1994_m_to_srl(m):
     """
-    See Wells & Coppersmith (1994).
+    Relation between magnitude and surface rupture length.
+
+    See Wells & Coppersmith (1994) p. 990 (r=0.89).
     """
-    return (10**(-4.45 + 0.63 * m), 0.33)
+    return (1000*10**(-3.22 + 0.69 * m), 0.22)
 
 
-def WC1994A_m_to_ad(m):
+def WC1994_ad_to_m(ad):
     """
-    See Wells & Coppersmith (1994).
+    Relation between average displacement and magnitude.
+
+    See Wells & Coppersmith (1994) p. 991 (r=0.75).
+    """
+    return (6.93 + 0.82 * np.log10(ad), 0.39)
+
+
+def WC1994_m_to_ad(m):
+    """
+    Relation between magnitude and average displacement.
+
+    See Wells & Coppersmith (1994) p. 991 (r=0.75).
     """
     return (10**(-4.80 + 0.69 * m), 0.36)
 
 
-def WC1994N_m_to_md(m):
+def WC1994_md_to_m(md):
     """
-    See Wells & Coppersmith (1994).
+    Relation between maximum displacement and magnitude.
+
+    See Wells & Coppersmith (1994) p. 991 (r=0.78).
     """
-    return (10**(-5.90 + 0.89 * m), 0.38)
+    return (6.69 + 0.74 * np.log10(md), 0.40)
 
 
-def WC1994A_m_to_md(m):
+def WC1994_m_to_md(m):
     """
-    See Wells & Coppersmith (1994).
+    Relation between magnitude and maximum displacement.
+
+    See Wells & Coppersmith (1994) p. 991 (r=0.78).
     """
     return (10**(-5.46 + 0.82 * m), 0.42)
 
 
-def WC1994N_rl_to_ad(rl):
+def SommervilleEtAl1999_a_to_m(a):
     """
-    See Wells & Coppersmith (1994).
+    Relation between area and magnitude.
+
+    See Sommerville et al. (1999) p. 70.
     """
-    return (10**(-1.99 + 1.24 * np.log10(rl/1000)), 0.37)
+    return np.log10(a*10**6) + 3.95
 
 
-def WC1994A_rl_to_ad(rl):
+def SommervilleEtAl1999_m_to_ad(m):
     """
-    See Wells & Coppersmith (1994).
+    Relation between magnitude and average displacement.
+
+    See Sommerville et al. (1999) p. 70.
     """
-    return (10**(-1.43 + 0.88 * np.log10(rl/1000)), 0.36)
+    return 1.56 * 10**-7 * (10**7 * mw_to_m0(m))**(1/3) / 100
 
 
-def WC1994N_rl_to_md(rl):
+def Wesnousky2008_srl_to_m(srl):
     """
-    See Wells & Coppersmith (1994).
     """
-    return (10**(-1.98 + 1.51 * np.log10(rl/1000)), 0.41)
-
-
-def WC1994A_rl_to_md(rl):
-    """
-    See Wells & Coppersmith (1994).
-    """
-    return (10**(-1.38 + 1.02 * np.log10(rl/1000)), 0.41)
+    return 5.30 + 1.02 * np.log10(srl/1000)
