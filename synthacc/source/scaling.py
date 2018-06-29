ef9942a7e486bc5cca603a305eb95a263a3c9115
Bart Vleminckx <bart.vleminckx@observatory.be>
2018-06-29 09:43:01 +0200

    Review of 'source.scaling' module

--- synthacc/source/scaling.py
+++ synthacc/source/scaling.py
@@ -26,6 +26,24 @@ def WC1994_m_to_srl(m):
     return (1000*10**(-3.22 + 0.69 * m), 0.22)
 
 
+def WC1994_a_to_m(a):
+    """
+    Relation between area and magnitude.
+
+    See Wells & Coppersmith (1994) p. 990 (r=0.95).
+    """
+    return (4.07 + 0.98 * np.log10(a/10**6), 0.24)
+
+
+def WC1994_m_to_a(m):
+    """
+    Relation between magnitude and area.
+
+    See Wells & Coppersmith (1994) p. 990 (r=0.95).
+    """
+    return 10**6 * 10**(-3.49 + 0.91 * m)
+
+
 def WC1994_ad_to_m(ad):
     """
     Relation between average displacement and magnitude.
@@ -62,13 +80,13 @@ def WC1994_m_to_md(m):
     return (10**(-5.46 + 0.82 * m), 0.42)
 
 
-def SommervilleEtAl1999_a_to_m(a):
+def SommervilleEtAl1999_m_to_a(m):
     """
-    Relation between area and magnitude.
+    Relation between magnitude and area.
 
     See Sommerville et al. (1999) p. 70.
     """
-    return np.log10(a*10**6) + 3.95
+    return (2.23 * 10**-15 * (10**7 * mw_to_m0(m))**(2/3) * 10**6, None)
 
 
 def SommervilleEtAl1999_m_to_ad(m):
@@ -77,10 +95,13 @@ def SommervilleEtAl1999_m_to_ad(m):
 
     See Sommerville et al. (1999) p. 70.
     """
-    return 1.56 * 10**-7 * (10**7 * mw_to_m0(m))**(1/3) / 100
+    return (1.56 * 10**-7 * (10**7 * mw_to_m0(m))**(1/3) / 100, None)
 
 
 def Wesnousky2008_srl_to_m(srl):
     """
+    Relation between surface rupture length and magnitude.
+
+    See Wesnousky (2008) p. 1620 (r=0.82).
     """
-    return 5.30 + 1.02 * np.log10(srl/1000)
+    return (5.30 + 1.02 * np.log10(srl/1000), 0.28)
--- tests/test_source/test_scaling.py
+++ tests/test_source/test_scaling.py
@@ -6,6 +6,6 @@ Tests for 'source.scaling' module.
 import unittest
 
 from synthacc.source.scaling import (WC1994_srl_to_m, WC1994_m_to_srl,
-    WC1994_ad_to_m, WC1994_m_to_ad, WC1994_md_to_m, WC1994_m_to_md,
-    SommervilleEtAl1999_a_to_m, SommervilleEtAl1999_m_to_ad,
-    Wesnousky2008_srl_to_m)
+    WC1994_a_to_m, WC1994_m_to_a, WC1994_ad_to_m, WC1994_m_to_ad,
+    WC1994_md_to_m, WC1994_m_to_md, SommervilleEtAl1999_m_to_a,
+    SommervilleEtAl1999_m_to_ad, Wesnousky2008_srl_to_m)
