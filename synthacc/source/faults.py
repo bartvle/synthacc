5e83b8ab32f5b9a959d503474ce6431c46ac5e95
Bart Vleminckx <bart.vleminckx@observatory.be>
2018-09-12 14:21:40 +0200

    Review of 'source.faults' module

--- synthacc/source/faults.py
+++ synthacc/source/faults.py
@@ -3,33 +3,39 @@ The 'source.faults' module.
 """
 
 
-import matplotlib.pyplot as plt
-import numpy as np
-
-from ..apy import Object, is_pos_number
-from ..earth.flat import Rectangle
-from .moment import calculate as calculate_moment, m0_to_mw
+from ..apy import is_non_neg_number, is_pos_number
+from ..earth import flat as earth
+# from .moment import calculate as calculate_moment, m0_to_mw
 
 
 ## Average rigidity (in Pa) in crust (from USGS website)
 RIGIDITY = 3.2 * 10**10
 
 
-class SingularFault(Rectangle):
+class SimpleFault(earth.SimpleSurface):
     """
     A fault with a rectangular surface.
     """
 
-    def __init__(self, x1, y1, x2, y2, upper_depth, lower_depth, dip, rigidity=RIGIDITY, validate=True):
+    def __init__(self, x1, y1, x2, y2, upper_depth, lower_depth, dip, rigidity=RIGIDITY, upper_sd=None, lower_sd=None, validate=True):
         """
         """
         super().__init__(
             x1, y1, x2, y2, upper_depth, lower_depth, dip, validate=validate)
 
+        if upper_sd is None:
+            upper_sd = upper_depth
+        if lower_sd is None:
+            lower_sd = lower_depth
+
         if validate is True:
             assert(is_pos_number(rigidity))
+            assert(is_non_neg_number(upper_sd) and upper_sd >= upper_depth)
+            assert(is_non_neg_number(lower_sd) and lower_sd <= lower_depth)
 
         self._rigidity = rigidity
+        self._upper_sd = upper_sd
+        self._lower_sd = lower_sd
 
     @property
     def rigidity(self):
@@ -39,103 +45,115 @@ class SingularFault(Rectangle):
         return self._rigidity
 
     @property
-    def rectangle(self):
+    def upper_sd(self):
         """
         """
-        r = Rectangle(
-            self._x1, self._y1,
-            self._x2, self._y2,
-            self._upper_depth,
-            self._lower_depth,
-            self._dip,
-            self._rigidity)
-
-        return r
-
-    def get_max_moment(self, slip, validate=True):
-        """
-        slip: pos number, slip (in m)
-
-        return: pos number, maximum seismic moment
-        """
-        return calculate_moment(self.area, slip, self.rigidity, validate)
-
-    def get_max_magnitude(self, slip, validate=True):
-        """
-        slip: pos number, slip (in m)
-
-        return: number, maximum moment magnitude
-        """
-        return m0_to_mw(self.get_max_moment(slip, validate), validate=False)
-
-
-class ComposedFault(Object):
-    """
-    A fault composed of multiple singular faults.
-    """
-
-    def __init__(self, faults, validate=True):
-        """
-        faults: list of 'faults.SingularFault' instances
-        """
-        if validate is True:
-            for f in faults:
-                assert(type(f) is SingularFault)
-
-        self._faults = faults
-
-    def __len__(self):
-        """
-        """
-        return len(self._faults)
-
-    def __getitem__(self, i):
-        """
-        """
-        assert(type(i) is int)
-        return self._faults[i]
-
-    def __iter__(self):
-        """
-        """
-        for f in self._faults:
-            yield f
+        return self._upper_sd
 
     @property
-    def length(self):
+    def lower_sd(self):
         """
-        return: pos number
         """
-        l = 0
-        for f in self:
-            l += f.length
-        return l
+        return self._lower_sd
 
-    @property
-    def area(self):
-        """
-        return: pos number
-        """
-        a = 0
-        for f in self:
-            a += f.area
-        return a
+#     @property
+#     def rectangle(self):
+#         """
+#         """
+#         r = Rectangle(
+#             self._x1, self._y1,
+#             self._x2, self._y2,
+#             self._upper_depth,
+#             self._lower_depth,
+#             self._dip,
+#             self._rigidity)
 
-    def get_max_moment(self, slip, validate=True):
-        """
-        slip: pos number, slip (in m)
+#         return r
 
-        return: pos number, maximum seismic moment
-        """
-        if validate is True:
-            assert(is_pos_number(slip))
+#     def get_max_moment(self, slip, validate=True):
+#         """
+#         slip: pos number, slip (in m)
 
-        return sum([f.get_max_moment(slip, validate=False) for f in self])
+#         return: pos number, maximum seismic moment
+#         """
+#         return calculate_moment(self.area, slip, self.rigidity, validate)
 
-    def get_max_magnitude(self, slip, validate=True):
-        """
-        slip: pos number, slip (in m)
+#     def get_max_magnitude(self, slip, validate=True):
+#         """
+#         slip: pos number, slip (in m)
 
-        return: number, maximum moment magnitude
-        """
-        return m0_to_mw(self.get_max_moment(slip, validate), validate=False)
+#         return: number, maximum moment magnitude
+#         """
+#         return m0_to_mw(self.get_max_moment(slip, validate), validate=False)
+
+
+# class ComposedFault(Object):
+#     """
+#     A fault composed of multiple simple faults.
+#     """
+
+#     def __init__(self, faults, validate=True):
+#         """
+#         faults: list of 'faults.SimpleFault' instances
+#         """
+#         if validate is True:
+#             for f in faults:
+#                 assert(type(f) is SimpleFault)
+
+#         self._faults = faults
+
+#     def __len__(self):
+#         """
+#         """
+#         return len(self._faults)
+
+#     def __getitem__(self, i):
+#         """
+#         """
+#         assert(type(i) is int)
+#         return self._faults[i]
+
+#     def __iter__(self):
+#         """
+#         """
+#         for f in self._faults:
+#             yield f
+
+#     @property
+#     def length(self):
+#         """
+#         return: pos number
+#         """
+#         l = 0
+#         for f in self:
+#             l += f.length
+#         return l
+
+#     @property
+#     def area(self):
+#         """
+#         return: pos number
+#         """
+#         a = 0
+#         for f in self:
+#             a += f.area
+#         return a
+
+#     def get_max_moment(self, slip, validate=True):
+#         """
+#         slip: pos number, slip (in m)
+
+#         return: pos number, maximum seismic moment
+#         """
+#         if validate is True:
+#             assert(is_pos_number(slip))
+
+#         return sum([f.get_max_moment(slip, validate=False) for f in self])
+
+#     def get_max_magnitude(self, slip, validate=True):
+#         """
+#         slip: pos number, slip (in m)
+
+#         return: number, maximum moment magnitude
+#         """
+#         return m0_to_mw(self.get_max_moment(slip, validate), validate=False)
--- tests/test_source/test_faults.py
+++ tests/test_source/test_faults.py
@@ -5,14 +5,32 @@ Tests for 'source.faults' module.
 
 import unittest
 
-from synthacc.source.faults import RIGIDITY, SingularFault, ComposedFault
+from synthacc.source.faults import RIGIDITY, SimpleFault
 
 
-class TestSingularFault(unittest.TestCase):
+class TestSimpleFault(unittest.TestCase):
     """
     """
 
     def test_properties(self):
         """
         """
-        sf = SingularFault(0, 0, 10000, 0, 1000, 20000, 60)
+        upper_depth = 10000
+        lower_depth = 20000
+        dip = 60
+        rigidity = 3.1 * 10**10
+        upper_sd = 12000
+        lower_sd = 18000
+        sf = SimpleFault(0, 0, 10000, 0, upper_depth, lower_depth, dip,
+            rigidity, upper_sd, lower_sd)
+        self.assertEqual(sf.upper_depth, upper_depth)
+        self.assertEqual(sf.lower_depth, lower_depth)
+        self.assertEqual(sf.dip, dip)
+        self.assertEqual(sf.rigidity, rigidity)
+        self.assertEqual(sf.upper_sd, upper_sd)
+        self.assertEqual(sf.lower_sd, lower_sd)
+
+        sf = SimpleFault(0, 0, 10000, 0, upper_depth, lower_depth, dip)
+        self.assertEqual(sf.rigidity, RIGIDITY)
+        self.assertEqual(sf.upper_sd, upper_depth)
+        self.assertEqual(sf.lower_sd, lower_depth)
