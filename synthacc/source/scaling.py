007bbf356de347703f5d85ba4ae7ff38d4541c62
Bart Vleminckx <bart.vleminckx@observatory.be>
2018-09-13 12:04:57 +0200

    Review of 'source.scaling' module

--- synthacc/source/scaling.py
+++ synthacc/source/scaling.py
@@ -6,6 +6,7 @@ The 'source.scaling' module.
 from abc import ABC, abstractmethod
 
 import numpy as np
+import scipy.stats
 
 from ..apy import Object
 from .moment import mw_to_m0
@@ -21,6 +22,7 @@ class ScalingRelationship(ABC, Object):
     OF = None
     TO = None
     SD = None
+    FACTOR = 1
 
     def __init__(self):
         """
@@ -35,10 +37,28 @@ class ScalingRelationship(ABC, Object):
         return None
 
     @abstractmethod
-    def sample(self, of):
+    def get_mean(self, to, n):
         """
         """
         return None
+
+    @abstractmethod
+    def get_sd(self, to, n):
+        """
+        """
+        return None
+
+    @abstractmethod
+    def sample(self, of, n):
+        """
+        """
+        return None
+
+    @property
+    def name(self):
+        """
+        """
+        return self.__class__.__name__
 
 
 class LinScalingRelationship(ScalingRelationship, ABC):
@@ -51,24 +71,34 @@ class LinScalingRelationship(ScalingRelationship, ABC):
         """
         return None
 
-    def sample(self, of):
+    def get_mean(self, of, validate=True):
+        """
+        """
+        return self.FACTOR * self(of)
+
+    def get_sd(self, of, n=1, validate=True):
+        """
+        """
+        to = self(of)
+        sd = self.SD * n
+        sd_min = self.FACTOR * (to-sd)
+        sd_max = self.FACTOR * (to+sd)
+
+        return (sd_min, sd_max)
+
+    def sample(self, of, n=2, validate=True):
         """
         """
         if self.SD is not None:
-            return np.random.normal(self(of), self.SD)
+            v = scipy.stats.truncnorm.rvs(-n, +n, self(of), self.SD)
+            return self.FACTOR * v
         else:
             return None
-
-    def get_sd(self, to, n=1, validate=True):
-        """
-        """
-        std = self.SD * n
-
-        return (to-std, to+std)
 
 
 class LogScalingRelationship(ScalingRelationship, ABC):
     """
+    Standard deviation does not change if scaling is power of 10.
     """
 
     @abstractmethod
@@ -77,38 +107,63 @@ class LogScalingRelationship(ScalingRelationship, ABC):
         """
         return None
 
-    def sample(self, of):
+    def get_mean(self, of, validate=True):
+        """
+        """
+        return self.FACTOR * 10**(self(of))
+
+    def get_sd(self, of, n=1, validate=True):
+        """
+        """
+        to = self(of)
+        sd = self.SD * n
+        sd_min = self.FACTOR * 10**(to-sd)
+        sd_max = self.FACTOR * 10**(to+sd)
+
+        return (sd_min, sd_max)
+
+    def sample(self, of, n=2, validate=True):
         """
         """
         if self.SD is not None:
-            return 10**(np.random.normal(np.log10(self(of)), self.SD))
+            v = scipy.stats.truncnorm.rvs(-n, +n, self(of), self.SD)
+            return self.FACTOR * 10**(v)
         else:
             return None
-
-    def get_sd(self, to, n=1, validate=True):
-        """
-        """
-        to = np.log10(to)
-        std = self.SD * n
-
-        return (10**(to-std), 10**(to+std))
 
 
 class WC1994_m2a(LogScalingRelationship):
     """
     Relation between magnitude and area.
 
-    See Wells & Coppersmith (1994) p. 990 (r=0.95).
+    See Wells & Coppersmith (1994) p. 990 (r=0.95, n=148).
     """
 
     OF, TO = 'm', 'a'
-
     SD = 0.24
+    FACTOR = 10**6
 
     def __call__(self, of):
         """
         """
-        return 10**6 * 10**(-3.49 + 0.91 * of)
+        return -3.49 + 0.91 * of
+
+
+class WC1994_m2a_n(LogScalingRelationship):
+    """
+    Relation between magnitude and area.
+
+    See Wells & Coppersmith (1994) p. 990 (r=0.92, n=22).
+    """
+
+    OF, TO = 'm', 'a'
+    SD = 0.22
+    FACTOR = 10**6
+
+    def __call__(self, of):
+        """
+        """
+        return -2.87 + 0.82 * of
 
 
 class WC1994_a2m(LinScalingRelationship):
@@ -119,7 +174,6 @@ class WC1994_a2m(LinScalingRelationship):
     """
 
     OF, TO = 'a', 'm'
-
     SD = 0.24
 
     def __call__(self, of):
@@ -132,17 +186,51 @@ class WC1994_m2sl(LogScalingRelationship):
     """
     Relation between magnitude and surface length.
 
-    See Wells & Coppersmith (1994) p. 990 (r=0.89).
+    See Wells & Coppersmith (1994) p. 990 (r=0.89, n=77).
     """
 
     OF, TO = 'm', 'sl'
-
     SD = 0.22
+    FACTOR = 10**3
 
     def __call__(self, of):
         """
         """
-        return 1000*10**(-3.22 + 0.69 * of)
+        return -3.22 + 0.69 * of
+
+
+class WC1994_m2l(LogScalingRelationship):
+    """
+    Relation between magnitude and surface length.
+
+    See Wells & Coppersmith (1994) p. 990 (r=0.94, n=167).
+    """
+
+    OF, TO = 'm', 'l'
+    SD = 0.22
+    FACTOR = 10**3
+
+    def __call__(self, of):
+        """
+        """
+        return -2.44 + 0.59 * of
+
+
+class WC1994_m2l_n(LogScalingRelationship):
+    """
+    Relation between magnitude and surface length.
+
+    See Wells & Coppersmith (1994) p. 990 (r=0.88, n=24).
+    """
+
+    OF, TO = 'm', 'l'
+    SD = 0.17
+    FACTOR = 10**3
+
+    def __call__(self, of):
+        """
+        """
+        return -1.88 + 0.50 * of
 
 
 class WC1994_sl2m(LinScalingRelationship):
@@ -153,7 +241,6 @@ class WC1994_sl2m(LinScalingRelationship):
     """
 
     OF, TO = 'sl', 'm'
-
     SD = 0.28
 
     def __call__(self, of):
@@ -170,13 +257,12 @@ class WC1994_m2as(LogScalingRelationship):
     """
 
     OF, TO = 'm', 'as'
-
     SD = 0.36
 
     def __call__(self, of):
         """
         """
-        return 10**(-4.80 + 0.69 * of)
+        return -4.80 + 0.69 * of
 
 
 class WC1994_as2m(LinScalingRelationship):
@@ -187,7 +273,6 @@ class WC1994_as2m(LinScalingRelationship):
     """
 
     OF, TO = 'as', 'm'
-
     SD = 0.39
 
     def __call__(self, of):
@@ -204,13 +289,12 @@ class WC1994_m2ms(LogScalingRelationship):
     """
 
     OF, TO = 'm', 'ms'
-
     SD = 0.42
 
     def __call__(self, of):
         """
         """
-        return 10**(-5.46 + 0.82 * of)
+        return -5.46 + 0.82 * of
 
 
 class WC1994_ms2m(LinScalingRelationship):
@@ -221,7 +305,6 @@ class WC1994_ms2m(LinScalingRelationship):
     """
 
     OF, TO = 'ms', 'm'
-
     SD = 0.40
 
     def __call__(self, of):
@@ -238,13 +321,13 @@ class SommervilleEtAl1999_m2a(LinScalingRelationship):
     """
 
     OF, TO = 'm', 'a'
-
     SD = None
+    FACTOR = 10**6
 
     def __call__(self, of):
         """
         """
-        return 2.23 * 10**-15 * (10**7 * mw_to_m0(of))**(2/3) * 10**6
+        return 2.23 * 10**-15 * (10**7 * mw_to_m0(of))**(2/3)
 
 
 class SommervilleEtAl1999_m2as(LinScalingRelationship):
@@ -255,13 +338,13 @@ class SommervilleEtAl1999_m2as(LinScalingRelationship):
     """
 
     OF, TO = 'm', 'as'
-
     SD = None
+    FACTOR = 10**-2
 
     def __call__(self, of):
         """
         """
-        return 1.56 * 10**-7 * (10**7 * mw_to_m0(of))**(1/3) / 100
+        return 1.56 * 10**-7 * (10**7 * mw_to_m0(of))**(1/3)
 
 
 class Wesnousky2008_sl2m(LinScalingRelationship):
@@ -272,13 +355,40 @@ class Wesnousky2008_sl2m(LinScalingRelationship):
     """
 
     OF, TO = 'sl', 'm'
-
     SD = 0.28
 
     def __call__(self, of):
         """
         """
         return 5.30 + 1.02 * np.log10(of/1000)
+
+
+class Leonard2014_m2a_scr_ds(LogScalingRelationship):
+    """
+    """
+
+    OF, TO = 'm', 'l'
+    SD = None
+    FACTOR = 10**6
+
+    def __call__(self, of):
+        """
+        """
+        return (of - 4.19) / 1.00
+
+
+class Leonard2014_m2l_scr_ds(LogScalingRelationship):
+    """
+    """
+
+    OF, TO = 'm', 'l'
+    SD = None
+    FACTOR = 10**3
+
+    def __call__(self, of):
+        """
+        """
+        return (of - 4.32) / 1.667
 
 
 class ThingbaijamEA2017_m2a_n(LogScalingRelationship):
@@ -289,13 +399,13 @@ class ThingbaijamEA2017_m2a_n(LogScalingRelationship):
     """
 
     OF, TO = 'm', 'a'
-
     SD = 0.181
+    FACTOR = 10**6
 
     def __call__(self, of):
         """
         """
-        return 10**(-2.551 + 0.808 * of) * 10**6
+        return -2.551 + 0.808 * of
 
 
 class ThingbaijamEA2017_m2l_n(LogScalingRelationship):
@@ -306,13 +416,13 @@ class ThingbaijamEA2017_m2l_n(LogScalingRelationship):
     """
 
     OF, TO = 'm', 'l'
-
     SD = 0.128
+    FACTOR = 10**3
 
     def __call__(self, of):
         """
         """
-        return 10**(-1.722 + 0.485 * of) * 10**3
+        return -1.722 + 0.485 * of
 
 
 class ThingbaijamEA2017_m2as_n(LogScalingRelationship):
@@ -323,10 +433,9 @@ class ThingbaijamEA2017_m2as_n(LogScalingRelationship):
     """
 
     OF, TO = 'm', 'as'
-
     SD = 0.195
 
     def __call__(self, of):
         """
         """
-        return 10**(-4.967 + 0.693 * of)
+        return -4.967 + 0.693 * of
--- tests/test_source/test_scaling.py
+++ tests/test_source/test_scaling.py
@@ -5,7 +5,12 @@ Tests for 'source.scaling' module.
 
 import unittest
 
-from synthacc.source.scaling import (WC1994_m2a, WC1994_a2m, WC1994_m2sl,
-    WC1994_sl2m, WC1994_m2as, WC1994_as2m, WC1994_m2ms, WC1994_ms2m,
-    SommervilleEtAl1999_m2a, SommervilleEtAl1999_m2as, Wesnousky2008_sl2m,
-    ThingbaijamEA2017_m2a_n, ThingbaijamEA2017_m2l_n, ThingbaijamEA2017_m2as_n)
+from synthacc.source.scaling import (WC1994_m2a, WC1994_m2a_n, WC1994_a2m,
+    WC1994_m2sl, WC1994_m2l, WC1994_m2l_n, WC1994_sl2m, WC1994_m2as,
+    WC1994_as2m, WC1994_m2ms, WC1994_ms2m, SommervilleEtAl1999_m2a,
+    SommervilleEtAl1999_m2as, Wesnousky2008_sl2m, Leonard2014_m2a_scr_ds,
+    Leonard2014_m2l_scr_ds, ThingbaijamEA2017_m2a_n, ThingbaijamEA2017_m2l_n,
+    ThingbaijamEA2017_m2as_n)
+
+
+#TODO: implement test
