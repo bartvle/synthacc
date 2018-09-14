6867a1ab2b078240335a9a48ee0f689891a4aef3
Bart Vleminckx <bart.vleminckx@observatory.be>
2018-09-14 10:24:51 +0200

    Review of 'source.rupture.velocity' module

--- tests/test_source/test_rupture/test_velocity.py
+++ tests/test_source/test_rupture/test_velocity.py
@@ -6,5 +6,25 @@ Tests for 'source.rupture.propagation' module.
 import unittest
 
 from synthacc.source.rupture.velocity import (VelocityDistribution,
-    RFVelocityDistributionGenerator, GP2010VelocityDistributionCalculator,
-    GP2010VelocityDistributionGenerator, GP2016VelocityDistributionGenerator)
+    ConstantVDC, RandomFieldVDC)
+
+
+class TestVelocityDistribution(unittest.TestCase):
+    """
+    #TODO: implement test
+    """
+    pass
+
+
+class TestConstantVDC(unittest.TestCase):
+    """
+    #TODO: implement test
+    """
+    pass
+
+
+class TestRandomFieldVDC(unittest.TestCase):
+    """
+    #TODO: implement test
+    """
+    pass
