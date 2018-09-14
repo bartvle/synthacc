e07180f8c3f69d2d45952d44a7146017e5ebdbda
Bart Vleminckx <bart.vleminckx@observatory.be>
2018-09-14 10:26:09 +0200

    Update tests

--- tests/test_source/test_rupture/test_propagation.py
+++ tests/test_source/test_rupture/test_propagation.py
@@ -7,3 +7,24 @@ import unittest
 
 from synthacc.source.rupture.propagation import (TravelTimes,
     ConstantVelocityTravelTimeCalculator, TravelTimeCalculator)
+
+
+class TestTravelTimes(unittest.TestCase):
+    """
+    #TODO: implement test
+    """
+    pass
+
+
+class TestConstantVelocityTravelTimeCalculator(unittest.TestCase):
+    """
+    #TODO: implement test
+    """
+    pass
+
+
+class TestTravelTimeCalculator(unittest.TestCase):
+    """
+    #TODO: implement test
+    """
+    pass
