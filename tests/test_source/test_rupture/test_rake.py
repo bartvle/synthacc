5b8c66dfe01b4e7242fb0569c79afddabb39026f
Bart Vleminckx <bart.vleminckx@observatory.be>
2018-09-14 09:09:36 +0200

    Add 'source.rupture.rake' module

---
+++ tests/test_source/test_rupture/test_rake.py
@@ -0,0 +1,37 @@
+"""
+Tests for 'source.rupture.rake' module.
+"""
+
+
+import unittest
+
+from synthacc.source.rupture.rake import (RakeDistribution,
+    RakeDistributionCalculator, ConstantRDC, RandomFieldRDC)
+
+
+class TestRakeDistribution(unittest.TestCase):
+    """
+    #TODO: implement test
+    """
+    pass
+
+
+class TestRakeDistributionCalculator(unittest.TestCase):
+    """
+    #TODO: implement test
+    """
+    pass
+
+
+class TestConstantRDC(unittest.TestCase):
+    """
+    #TODO: implement test
+    """
+    pass
+
+
+class TestRandomFieldRDC(unittest.TestCase):
+    """
+    #TODO: implement test
+    """
+    pass
