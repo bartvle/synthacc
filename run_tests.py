ed9f8933553c736a92c3a13d74505ee0a14f4473
Bart Vleminckx <bart.vleminckx@observatory.be>
2018-10-16 11:08:32 +0200

    Make test output folder if it doesn't exist

--- run_tests.py
+++ run_tests.py
@@ -12,6 +12,10 @@ OUTPUT_DIR = os.path.join(os.path.dirname(__file__), 'tests', 'output')
 
 if __name__ == '__main__':
 
+    ## make tests\output folder
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+
     ## empty tests\output folder
     for fn in os.listdir(OUTPUT_DIR):
         os.remove(os.path.join(OUTPUT_DIR, fn))
